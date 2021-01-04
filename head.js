b8e0d68878646f50e66eae05c447734d796e75eb
QuintenAiton <quinaiton@hotmail.com>
2021-01-04 22:35:58 +0000

    function that retreives the first item in an array

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = (actual, expected) => {
+  return actual === expected ?
+    `✅✅Assertion Passed: ${actual} === ${expected}  ` :
+    `❌❌ Assertion Failed : ${actual} != ${expected}`;
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('light', 'light');
+assertEqual(1, 4);
+
+
+const head = (arr) => {
+  return arr[0];
+}
+
