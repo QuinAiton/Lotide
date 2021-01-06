a10edc6479cbaa1512b6eea59d6091db6aa41d38
QuintenAiton <quinaiton@hotmail.com>
2021-01-06 17:32:15 +0000

    takes in an array and returns the item/items that are in the middle

---
+++ middle.js
@@ -0,0 +1,14 @@
+
+const middle = (arr) => {
+  let middle = [];
+  if (arr.length > 2) {
+    if (arr.length % 2 === 0) {
+      middle.push(arr.length / 2);
+      middle.push(arr.length / 2 + 1);
+    } else {
+      middle.push(Math.ceil(arr.length / 2))
+    }
+  }
+  return middle;
+
+}
