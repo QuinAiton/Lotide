106de060e586712c1fbdad874768192409ae6546
QuintenAiton <quinaiton@hotmail.com>
2021-01-06 23:15:20 +0000

    takes in a string and returns an object containing the letter and its associated indexes

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = (sentence) => {
+  let result = {};
+  const split = sentence.split('');
+  for (let i = 0; i < split.length; i++) {
+    if (result[split[i]]) {
+      result[split[i]].push(i);
+    } else {
+      result[split[i]] = [i];
+    }
+  }
+  return (result);
+};
+
+console.log(letterPositions("hello"));
