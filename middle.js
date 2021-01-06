607bd231755347f3094540a77d74c8f712dd8ed9
QuintenAiton <quinaiton@hotmail.com>
2021-01-06 17:53:13 +0000

    takes in an array and returns the item/items that are in the middle

--- middle.js
+++ middle.js
@@ -1,14 +1,31 @@
 
+
+const assertArrayEqual = (actual, expected) => {
+  for (let i = 0; i < actual.length; i++) {
+    for (let j = 0; j < expected.length; j++) {
+      if (expected[i] !== actual[i]) {
+        return `❌❌ Assertion Failed : ${actual} !== ${expected}`;
+      }
+    }
+  }
+  return `✅✅Assertion Passed: ${actual} === ${expected}  `;
+};
+
 const middle = (arr) => {
   let middle = [];
   if (arr.length > 2) {
     if (arr.length % 2 === 0) {
-      middle.push(arr.length / 2);
-      middle.push(arr.length / 2 + 1);
+      middle.push(arr[arr.length / 2 - 1]);
+      middle.push(arr[arr.length / 2]);
     } else {
-      middle.push(Math.ceil(arr.length / 2));
+      middle.push(arr[Math.floor(arr.length / 2)]);
     }
   }
   return middle;
 
 };
+
+assertArrayEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArrayEqual(middle([1, 2]), []);
+assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArrayEqual(middle(['sam', 'i', 'am', 'green']), ['i', 'am']);
