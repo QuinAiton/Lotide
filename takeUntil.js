0d1656d333fe25a159906a371bc124a7348eef19
QuintenAiton <quinaiton@hotmail.com>
2021-01-07 22:22:09 +0000

    takes in an array and callback func and returns elements untill a truthy value is found

---
+++ takeUntil.js
@@ -0,0 +1,17 @@
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+
+const takeUntill = (array, callback) => {
+  const result = [];
+  for (const item of array) {
+    if (callback(item)) {
+      return result;
+    } else {
+      result.push(item);
+    }
+  }
+};
+
+console.log(takeUntill(data1, (word) => word < 0));
+console.log(takeUntill(data2, (word) => word === ','));
