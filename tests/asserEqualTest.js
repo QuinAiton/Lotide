2fd22b39fef78f7d65b4def3cd6199625030ec78
QuintenAiton <quinaiton@hotmail.com>
2021-01-11 16:51:12 +0000

    created test directory to house all tests used for function in lotide

---
+++ tests/asserEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual')
+
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual('light', 'light'));
+console.log(assertEqual(1, 4));
